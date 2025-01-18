406dbdb0cca62a0a9e12a6cd6be43b442788a984
ahmedzakaria2003 <you@example.com>
2025-01-18 23:30:30 +0200

    q1

--- Assignment 1 c# with oop/Program.cs
+++ Assignment 1 c# with oop/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace Assignment_1_c__with_oop
 {
-    [Flags] 
-    //enum WeekDays:byte
-    //{
-    //    Monday = 1 , Tuesday = 2, Wednesday = 4, Thursday = 8 , Friday = 16 , Saturday = 32, sunday = 64 
+    //[Flags] 
+    ////enum WeekDays:byte
+    ////{
+    ////    Monday = 1 , Tuesday = 2, Wednesday = 4, Thursday = 8 , Friday = 16 , Saturday = 32, sunday = 64 
 
-    //}
+    ////}
 
     internal class Program
     {
