7a42f3db25feb6ce7752af0c7f18cd834e4b3e04
ahmedzakaria2003 <you@example.com>
2025-01-18 23:49:30 +0200

    q2

--- Assignment 1 c# with oop/Program.cs
+++ Assignment 1 c# with oop/Program.cs
@@ -1,6 +1,15 @@
 ﻿namespace Assignment_1_c__with_oop
 {
-    //[Flags] 
+    [Flags]
+
+
+    enum SeasOn
+    {
+        Spring, Summer, Autumn, Winter
+
+    }
+
+
     ////enum WeekDays:byte
     ////{
     ////    Monday = 1 , Tuesday = 2, Wednesday = 4, Thursday = 8 , Friday = 16 , Saturday = 32, sunday = 64 
@@ -11,13 +20,48 @@
     {
         static void Main(string[] args)
         {
-            //#region MyRegion 1 
+            #region MyRegion 1 
 
             //WeekDays Day = (WeekDays) 127 ;
 
             //Console.WriteLine(Day);
+            #endregion
 
-            //#endregion
+            #region MyRegion 2
+
+
+
+            Console.WriteLine("Please Enter The Month :");
+            string month = Console.ReadLine();
+            if (month == "march " || month == " april" || month == "may")
+            {
+                SeasOn season = SeasOn.Spring;
+                Console.WriteLine(season);
+            }
+
+
+            else if (month == "june " || month == " july" || month == "august")
+            {
+                SeasOn season = SeasOn.Summer;
+                Console.WriteLine(season);
+            }
+
+            else if (month == "septembar " || month == "october " || month == "november")
+            {
+                SeasOn season = SeasOn.Autumn;
+                Console.WriteLine(season);
+            }
+
+            else if(month == "decamber " || month == " january" || month == "feburary" )
+            {
+                SeasOn season = SeasOn.Winter;
+                Console.WriteLine(season);
+            }
+
+
+            #endregion
+
         }
     }
-}
+    }
+
